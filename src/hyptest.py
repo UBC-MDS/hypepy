08ff482e95080164f1f5098ebf3cc7ab122b225c
joesdesk <jomar.sastrillo@gmail.com>
2018-03-04 00:54:49 -0800

    Add empty functions to prepare for testing

---
+++ src/hyptest.py
@@ -0,0 +1,23 @@
+# Functions for the hypepy module
+
+def conf_int(data):
+    """
+    Computes the confidence interval of the mean from a population.
+    """
+
+    pass
+
+
+def hyp_test(data, mean_0):
+    """
+    Prints the t-statistic, p-value and a recommendation for rejecting or accepting the null hypothesis.
+    """
+
+    pass
+
+def t_test(data, mean_0):
+    """
+    Calculates a t-statistic of the data relative to a mean which may be specified.
+    """
+
+    pass
