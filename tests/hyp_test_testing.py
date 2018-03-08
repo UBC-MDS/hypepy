4b3514c9b64936b5ec2f4a2fed323d311a5020ca
joesdesk <jomar.sastrillo@gmail.com>
2018-03-08 11:42:09 -0800

    Removed function copy; instead, function imported from package

--- tests/hyp_test_testing.py
+++ tests/hyp_test_testing.py
@@ -1,17 +1,7 @@
 import numpy as np
 import pytest
-import hypepy
+from hypepy import hyp_test
 
-def hyp_test(data, mean_0):
-'''
-This function prints the p-value and a recommendation for
-rejecting or accepting the null hypothesis.
-
-Arguments: data: a 1-dimensional numpy array. Non-numerical values result in an error
-         mean_0: the mean (as a number) of the population under the null hypothesis. Default: 0.
-
-Returns: results: a summary in the form of a string.
-'''
 
 def hyp_test_integer(hyp_test):
 '''
