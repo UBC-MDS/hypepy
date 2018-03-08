ffaf9b8a11721c82b01af79b3da9ec1e5440ab1b
joesdesk <jomar.sastrillo@gmail.com>
2018-03-08 15:01:27 -0800

    Removed function copy again after improper merge

--- tests/test_conf_int.py
+++ tests/test_conf_int.py
@@ -1,14 +1,6 @@
 import numpy as np
 import pytest
-
-def conf_int(data):
-    '''
-    This function takes in an array and outputs the 95% confidence interval of the population mean.
-
-    Arguments: data: a 1D numpy array of a list of numbers. Non-numerical values result in an error.
-
-    Returns: interval: a 2-element 1D numpy array indicating the start and end of the 95% confidence interval.
-    '''
+from hypepy import conf_int
 
 
 def test_type():
