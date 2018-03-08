26b1263549202229f8d462cb2b797041a6b054d7
Siobhan McCarter <siobhan.mccarter@gmail.com>
2018-03-08 14:46:27 -0800

    Adding function file

--- hypepy/conf_int.py
+++ hypepy/conf_int.py
@@ -1,6 +1,29 @@
 def conf_int(data):
     """
-    Computes the confidence interval of the mean from a population.
+    This function takes in an array and outputs the 95% confidence interval of the population mean.
+
+    Arguments: data: a 1D numpy array of a list of numbers. Non-numerical values result in an error.
+
+    Returns: interval: a 2-element 1D numpy array indicating the start and end of the 95% confidence interval.
     """
 
-    pass
+    import numpy as np
+
+    # Doing some checks on the user input
+    if len(data) == 0:
+        raise ValueError("Input cannot be empty")
+
+    # Set up the parameters
+    mean = np.mean(data)
+
+    sd = np.std(data, ddof=1)
+
+    n = len(data)
+
+    lower = round((mean - (1.96*(sd/np.sqrt(n)))),2)
+
+    upper = round((mean + (1.96*(sd/np.sqrt(n)))),2)
+
+    conf = np.array([lower, upper])
+
+    return conf
