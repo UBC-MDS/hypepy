0f322a263904d20a3f0c3217a4d44056dc73acfc
Siobhan McCarter <siobhan.mccarter@gmail.com>
2018-03-08 14:53:35 -0800

    Changing error type in test file

--- tests/conf_int_testing.py
+++ tests/conf_int_testing.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pytest
 
-from hypepy import conf_int
+def conf_int(data):
+    '''
+    This function takes in an array and outputs the 95% confidence interval of the population mean.
+
+    Arguments: data: a 1D numpy array of a list of numbers. Non-numerical values result in an error.
+
+    Returns: interval: a 2-element 1D numpy array indicating the start and end of the 95% confidence interval.
+    '''
 
 
 def type_test(conf_int):
@@ -17,7 +24,7 @@ def type_test(conf_int):
     # Test array with NA value present
     NA_test = np.array([1, 2, 3, 4, np.nan])
 
-    with pytest.raises(TypeError):
+    with pytest.raises(ValueError):
         conf_int(NA_test)
 
 
