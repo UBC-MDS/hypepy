0feb03ed949b163ebbe8c95301b4ecf6fd91df75
Ivan-Despot <32108839+Ivan-Despot@users.noreply.github.com>
2018-03-09 19:27:29 -0800

    Update test_hyp_test.py

--- tests/test_hyp_test.py
+++ tests/test_hyp_test.py
@@ -3,22 +3,29 @@ import pytest
 from hypepy import hyp_test
 
 
-def test_type():
+def hyp_test_integer(hyp_test):
     '''
     Test if the input data is numeric, not a string or boolean.
     '''
     with pytest.raises(TypeError):
-        hyp_test("my data")
-        hyp_test(True)
+        hyp_test( data = "my data", mean_0=1, alpha=0.05)
 
-def test_missing_data():
+    with pytest.raises(TypeError):
+        hyp_test(data = True, mean_0=1, alpha=0.05)
+
+def hyp_test_missing(hyp_test):
     '''
     Test if the input contains missing values.
     '''
-    assert data == np.isnan(data)
+    data = np.array([0, np.nan, 2])
+    with pytest.raises(TypeError):
+        hyp_test(data, mean_0=1, alpha = 0.05)
+        assert data == np.isnan(data)
 
-def test_non_zero():
+def hyp_test_non_zero(hyp_test):
     '''
     Test that the length of the data is greater than 0.
     '''
-    assert len(hyp_test) > 0
+    with pytest.raises(ValueError):
+        data = np.array([])
+        hyp_test(data, mean_0=1, alpha=0.05)
