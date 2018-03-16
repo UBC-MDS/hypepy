075d43d9cc77660f0cb1dc1f6ef98e00b5798a2f
joesdesk <jomar.sastrillo@gmail.com>
2018-03-15 22:45:42 -0700

    Added setup file for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hypepy',
+      version='0.1',
+      description='A user-friendly and intuitive hypothesis testing package',
+      url='https://github.com/UBC-MDS/hypepy',
+      author='Ivan Despot, Siobhan McCarter, Joe Sastrillo',
+      packages=['hypepy'],
+      zip_safe=False)
+
+      
